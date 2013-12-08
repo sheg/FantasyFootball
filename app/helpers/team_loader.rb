b2967d25d6cd1f971c44e9d180f354b0bf026dba
myakub <my4273@gmail.com>
2013-12-08 15:44:51 -0500

    modify  json data folder structure

--- app/helpers/team_loader.rb
+++ app/helpers/team_loader.rb
@@ -2,8 +2,11 @@ class TeamLoader
 
   attr_accessor :teams, :players
 
+  $base_folder = File.join(Rails.root, 'lib', 'json_data')
+  $my_folder = 'nfl'
+
   def load_teams_via_fantasy_data
-    api_data = JSON.parse(File.open(File.join(Rails.root, 'lib', 'teams.json')).read)
+    api_data = JSON.parse(File.open(File.join($base_folder, $my_folder, 'teams.json')).read)
     @teams = api_data.map do |team|
       {
           name: team['FullName'],
