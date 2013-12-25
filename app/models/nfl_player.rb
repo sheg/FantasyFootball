2463186a7feba3ed77ef63b772ff4d4bc0cbf17d
myakub <my4273@gmail.com>
2013-12-25 04:43:41 -0500

    Added threading for processing player stats

--- app/models/nfl_player.rb
+++ app/models/nfl_player.rb
@@ -1,7 +1,9 @@
 class NflPlayer < ActiveRecord::Base
   has_many :season_teams, class_name: NflSeasonTeamPlayer, foreign_key: :player_id
   has_many :game_players, class_name: NflGamePlayer, foreign_key: :nfl_player_id
-  has_many :games, class_name: NflGame, foreign_key: :nfl_player_id, through: :game_players
+  has_many :games, class_name: NflGame, through: :game_players
+  has_many :teams, class_name: NflTeam, through: :game_players
+  has_many :positions, class_name: NflPosition, through: :game_players
   has_many :stats, class_name: NflGameStatMap, through: :game_players
 
   def full_name
