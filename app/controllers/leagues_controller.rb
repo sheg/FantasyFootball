8a45fd0ad5a12c760ebfddc7eeb3e7d7573e10ba
sheg <danforkosh@yahoo.com>
2014-01-01 22:41:42 -0500

    view enhancements

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,14 +1,16 @@
 class LeaguesController < ApplicationController
+  before_action :signed_in_user, except: [:index]
 
   def index
     @leagues = League.all.includes(:teams)
+    @user_leagues = current_user.leagues if signed_in?
   end
 
   def show
     @league = League.includes(:users).find_by(id: params[:id])
     redirect_to action: 'index' unless @league
 
-    @my_team = @league.users.find(current_user.id) if signed_in?
+    @user_team = Team.find_by(user_id: current_user.id, league_id: @league.id)
   end
 
   def new
@@ -21,6 +23,21 @@ class LeaguesController < ApplicationController
     @league = League.includes(games: [:home_team, :away_team]).find_by(id: params[:id])
     redirect_to action: 'index' unless @league
 
-    @my_team = @league.users.find(current_user.id) if signed_in?
+    @user_team = Team.find_by(user_id: current_user.id, league_id: @league.id)
+  end
+
+  def join
+    redirect_to action: 'index' #for now...
+  end
+
+  def team
+    @league = League.includes(:users).find_by(id: params[:league_id])
+    @my_team = @league.users.find(current_user.id).inspect
   end
 end
+
+private
+
+  def signed_in_user
+    redirect_to signin_url, notice: "Please sign in" unless signed_in?
+  end
