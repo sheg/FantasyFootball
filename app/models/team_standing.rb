b823a633c84bc44d62e34d6369440dfb09e19445
myakub <my4273@gmail.com>
2014-02-04 23:49:37 -0500

    Add stored procedures for point calcs

--- app/models/team_standing.rb
+++ app/models/team_standing.rb
@@ -4,5 +4,11 @@ class TeamStanding < ActiveRecord::Base
   self.primary_keys = [ :team_id, :week ]
 
   belongs_to :team
-  belongs_to :league, through: :team
+  has_one :league, through: :team
+
+  scope :for_league_week, ->(l, w) { joins(:team).where('week = ? and league_id = ?', w, l).order('percent desc') }
+
+  def game
+    Game.find_by("week = #{self.week} and (home_team_id = #{self.team_id} or away_team_id = #{self.team_id})")
+  end
 end
