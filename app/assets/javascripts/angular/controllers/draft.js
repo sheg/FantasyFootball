7b155322bd8abf3259a3a77977d22da0bfe0f914
sheg <danforkosh@yahoo.com>
2014-08-14 01:55:20 -0400

    Merge branch 'master' of https://github.com/sheg/FantasyFootball into leagues_client
    Conflicts:
    	Gemfile
    	spec/factories/factories.rb

--- app/assets/javascripts/angular/controllers/draft.js
+++ app/assets/javascripts/angular/controllers/draft.js
@@ -2,31 +2,16 @@
 
   var app = angular.module('drafts', []);
 
-  app.factory('Players', function() {
-    return [
-      {
-        'first_name': 'Eli',
-        'last_name': 'Manning',
-        'position': 'QB',
-        'team': 'NYG',
-        'BYE': '5'
-      },
-      {
-        'first_name': 'Alex',
-        'last_name': 'Smith',
-        'position': 'QB',
-        'team': 'KC',
-        'BYE': '4'
-      },
-      {
-        'first_name': 'Frank',
-        'last_name': 'Gore',
-        'position': 'RB',
-        'team': 'SF',
-        'BYE': '8'
-      }
-    ];
+  app.factory('Players', function($http) {
+    var model = this;
+
+    $http.get("http://api.pointsleaders-dev.com:3000/v1/players").success(function(data) {
+      return model.playerData = data;
+    });
+
+    return model;
   });
+
 
   app.controller('DraftPlayersController', ['$scope', 'Players', function($scope, players) {
     $scope.players = players;
@@ -46,37 +31,3 @@
   }]);
 
 })();
-
-
-
-//app.controller('LeadsController', ['$http', '$scope', function($http, $scope) {
-//  $scope.lead = {};
-//
-//  $http.get("/v1/leads").success(function(data) {
-//    $scope.leads = data;
-//  });
-//
-//  $scope.addLead = function() {
-//    $http.post('/v1/leads', { name: $scope.lead.name, email: $scope.lead.email }).success(function(data) {
-//      $scope.leads.push(data);
-//      $scope.lead = {};
-//    });
-//  };
-//
-//  $scope.deleteLead = function(leadIndex) {
-//    var lead = $scope.leads[leadIndex];
-//    var _confirm = confirm("Are you sure you want to delete " + lead.name + "?");
-//    if(_confirm) {
-//      $http.delete("/v1/leads/" + lead.id).success(function() {
-//        $scope.leads.splice(leadIndex, 1);
-//      });
-//    }
-//  };
-//
-//  $scope.clearLead = function() {
-//    $scope.lead = {};
-//    $('#new-lead-name').val('');
-//    $('#new-lead-email').val('');
-//    $scope.addLeadForm.$setPristine();
-//  };
-//}]);
--- app/controllers/api/players_controller.rb
+++ app/controllers/api/players_controller.rb
@@ -2,8 +2,10 @@ module Api
   class PlayersController < ApplicationController
 
     def index
-      nfl_players = NflGamePlayer.all
-      render json: nfl_players, status: :ok
+      nfl_players = NflGamePlayer.includes([:player, :team, :position]).all.limit(30)
+      render json: nfl_players.to_json(:include => [{ :player => { :only => [:first_name, :last_name] }},
+                                                      :team => { :only => [:name, :abbr]},
+                                                      :position => { :only => :abbr }]), status: :ok
     end
   end
 end
